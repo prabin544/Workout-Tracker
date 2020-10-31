5d58a090fddd0c4e1fcae74ddf92d5ab69c88009
prabin singh <pravin544@gmail.com>
2020-10-31 15:45:59 -0500

    deploy in heroku

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./public"));
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // routes
-require("./routes/htmlRoutes.js")(app);
-app.use(require("./routes/apiRoutes.js"))
-
-
-// const htmlRoutes = require("./routes/htmlRoutes.js");
-// htmlRoutes(app);
-// const apiRoutes = require("./routes/apiRoutes.js");
-// apiRoutes(app);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-
+app.use(require("./routes/apiRoute.js"));
+app.use(require("./routes/htmlRoute.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
